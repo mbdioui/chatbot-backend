4d699e7d88159504a72372cb115908a1b3cd857b
bdiouimedsalah@gmail.com <Arij16072016>
2017-09-25 17:02:50 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$method= $_server['REQUEST_METHOD'];
+if($method=="POST")
+{
+	$requestbody =file_get_contents('php://input');
+	$json = json_decode($requestbody);
+	
+	$text= $json->result->parameters->text;
+	switch($text)
+	{
+	case 'hi':
+		$speech = "hi nice to meet you";
+		break;
+	case 'bye':
+		$speech= "bye ,good night";
+		break;
+	case 'anything':
+		$speech="yes you can type anything"
+		break;
+	default:
+		$speech ="sorry, i didnt get that"
+		break;
+		}
+	$response = new \stdClass();
+	$response->speech="";
+	$response->displayText="";
+	$response->source="webhook";
+	echo json_encode($response);
+}else
+	echo "method not allowed"
+
+?>
